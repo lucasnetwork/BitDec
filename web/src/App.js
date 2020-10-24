529b99e9da5883bbeb73d9542b19c336294d19af
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-10-23 22:41:32 -0300

    add ResultText in App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ResultText from './components/ResultText';
 
 function App() {
-	return <div className="App" />;
+	const [type, setType] = useState('binary');
+	const [value, setValue] = useState();
+	return (
+		<div className="App">
+			<ResultText value={value} />
+		</div>
+	);
 }
 
 export default App;
