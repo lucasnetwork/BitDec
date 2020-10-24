11733078ef1dff6c7e086a57cc38d6ceb7bf652e
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-10-24 12:29:56 -0300

    add font Roboto

--- web/src/components/ResultText/styles.js
+++ web/src/components/ResultText/styles.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export default styled.span`
-	/* font-family: Roboto; */
 	text-align: center;
 	font-size: 64px;
 	color: #212227;
--- web/src/globalStyles.js
+++ web/src/globalStyles.js
@@ -5,6 +5,7 @@ export default createGlobalStyle`
 		box-sizing:border-box;
 		margin:0;
 		padding:0;
+		font-family:'Roboto',Arial, Helvetica, sans-serif
 	}
 
 	html,body,#root{
