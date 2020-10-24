e9c8ecbe770ebabf3e1bd5dc51b0b6bd27f8fdc9
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-10-23 22:40:38 -0300

    add ResultText component

---
+++ web/src/components/ResultText/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Container from './styles';
+
+const ResultText = ({ value }) => {
+	return <Container>{value}</Container>;
+};
+
+export default ResultText;
---
+++ web/src/components/ResultText/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export default styled.span`
+	/* font-family: Roboto; */
+	font-size: 64px;
+	color: #212227;
+`;
