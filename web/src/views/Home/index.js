a8b9405281156855bce7026d15ebc97184b17113
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-10-24 11:10:50 -0300

    add function handlebinary and formatnumber

--- web/src/views/Home/index.js
+++ web/src/views/Home/index.js
@@ -1,17 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import ResultText from '../../components/ResultText';
 import Container, { ContainerButtons, ContainerText } from './styles';
 import Button from '../../components/Button';
 
+const typesShemas = {
+	binary(value) {
+		return value.match(/[^0-1]/);
+	},
+	hex(value) {
+		return value.match(/[g-z]/i);
+	},
+};
+
 const Home = () => {
-	const [type, setType] = useState('binary');
-	const [value, setValue] = useState('test');
+	const [type, setType] = useState('hex');
+	const [value, setValue] = useState('');
+	const [valueFormat, setValueFormat] = useState();
+
+	function formatNumber(e) {
+		if (typesShemas[type](e.target.value)) {
+			return;
+		}
+		setValue(e.target.value);
+	}
+	function handleBinary() {
+		const newValue = value.split('');
+		const valuebinary = newValue.reduce((acc, act, index) => {
+			return acc + act * Math.pow(2, newValue.length - index - 1);
+		}, 0);
+		setValueFormat(valuebinary);
+	}
+	useEffect(() => {
+		handleBinary();
+	}, [value]);
 	return (
 		<Container>
-			<ResultText value={value} />
+			<ResultText value={valueFormat} />
 			<ContainerText>
-				<input />
+				<input value={value} onChange={formatNumber} />
 				<ContainerButtons>
 					<Button value="binário" />
 					<Button value="hexa" />
