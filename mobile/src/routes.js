7098bd1a5cdc721db8276a9ff7cd5c24e8147cc3
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-10-27 14:36:28 -0300

    add Store View

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -3,13 +3,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './views/Home';
+import Store from './views/Store';
 
 const Stack = createStackNavigator();
 
 const Routes = () => (
 	<NavigationContainer>
-		<Stack.Navigator screenOptions={{ header: false }}>
+		<Stack.Navigator screenOptions={{ headerShown: false }}>
 			<Stack.Screen name="Home" component={Home} />
+			<Stack.Screen name="Store" component={Store} />
 		</Stack.Navigator>
 	</NavigationContainer>
 );
