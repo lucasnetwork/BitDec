0d7e98b8f3d961c29ce6da2cf1c3b5375be9d98b
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-10-27 14:22:06 -0300

    add routes

---
+++ mobile/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './views/Home';
+
+const Stack = createStackNavigator();
+
+const Routes = () => (
+	<NavigationContainer>
+		<Stack.Navigator screenOptions={{ header: false }}>
+			<Stack.Screen name="Home" component={Home} />
+		</Stack.Navigator>
+	</NavigationContainer>
+);
+
+export default Routes;
