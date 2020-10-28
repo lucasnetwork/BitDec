18797a2ebb8447112483a82e5f38a70836cddb68
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-10-27 22:00:02 -0300

    finished layout of store

--- mobile/src/views/Store/index.js
+++ mobile/src/views/Store/index.js
@@ -1,21 +1,55 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
+import { AntDesign, EvilIcons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import ContainerBigView from '../../Containers/BigView';
-import { ContainerButton } from './styles';
+import ContainerScroll, {
+	ContainerButton,
+	ValueText,
+	ContainerValues,
+	TextTitle,
+	ContainerValue,
+} from './styles';
 
 const Store = () => {
 	const navigation = useNavigation();
 	return (
-		<ContainerBigView>
-			<ContainerButton>
-				<TouchableOpacity onPress={() => navigation.navigate('Home')}>
-					<AntDesign name="arrowleft" size={40} color="#4A4343" />
-				</TouchableOpacity>
-			</ContainerButton>
-			<Text>Store</Text>
-		</ContainerBigView>
+		<ContainerScroll>
+			<ContainerBigView>
+				<ContainerButton>
+					<TouchableOpacity onPress={() => navigation.navigate('Home')}>
+						<AntDesign name="arrowleft" size={40} color="#4A4343" />
+					</TouchableOpacity>
+					<TextTitle>Store</TextTitle>
+				</ContainerButton>
+				<ContainerValues>
+					<ContainerValue>
+						<ValueText>Resul</ValueText>
+						<TouchableOpacity>
+							<EvilIcons name="trash" size={40} color="#B43F3F" />
+						</TouchableOpacity>
+					</ContainerValue>
+					<ContainerValue>
+						<ValueText>Resul</ValueText>
+						<TouchableOpacity>
+							<EvilIcons name="trash" size={40} color="#B43F3F" />
+						</TouchableOpacity>
+					</ContainerValue>
+					<ContainerValue>
+						<ValueText>Resul</ValueText>
+						<TouchableOpacity>
+							<EvilIcons name="trash" size={40} color="#B43F3F" />
+						</TouchableOpacity>
+					</ContainerValue>
+					<ContainerValue>
+						<ValueText>Resul</ValueText>
+						<TouchableOpacity>
+							<EvilIcons name="trash" size={40} color="#B43F3F" />
+						</TouchableOpacity>
+					</ContainerValue>
+				</ContainerValues>
+			</ContainerBigView>
+		</ContainerScroll>
 	);
 };
 
--- mobile/src/views/Store/styles.js
+++ mobile/src/views/Store/styles.js
@@ -1,7 +1,41 @@
 import styled from 'styled-components/native';
 
+export default styled.ScrollView`
+	flex: 1;
+	background-color: #8693ab;
+`;
+
 export const ContainerButton = styled.View`
-	justify-content: flex-start;
+	justify-content: space-between;
+	flex-direction: row;
 	width: 100%;
-	padding-left: 20px;
+	padding-left: 28px;
+	padding-right: 28px;
+	align-items: center;
+`;
+
+export const ValueText = styled.Text`
+	font-size: 32px;
+	color: #4a4343;
+`;
+
+export const ContainerValues = styled.View`
+	flex: 1;
+	width: 100%;
+`;
+
+export const ContainerValue = styled.View`
+	border-bottom-color: #c4c4c4;
+	border-bottom-width: 2px;
+	width: 100%;
+	justify-content: space-between;
+	padding-left: 16px;
+	padding-right: 16px;
+	margin-top: 28px;
+	flex-direction: row;
+`;
+
+export const TextTitle = styled.Text`
+	font-size: 48px;
+	color: #212227;
 `;
